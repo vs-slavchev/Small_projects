35910db6a1c803dd1a951f7f86afbfd50f58eb47
Veselin Slavchev <vs_slavchev@abv.bg>
2026-06-19 19:08:42 +0300

    Update FE and lambda to new variables

--- garden-bot/front-end/index.js
+++ garden-bot/front-end/index.js
@@ -1,17 +1,17 @@
 var timeLabels = [0, 0];
 var batteryValues = [0, 0];
-var moisture1Values = [0, 0];
-var moisture2Values = [0, 0];
+var moistureValues = [0, 0];
+var waterAvailableValues = [0, 0];
 var wateredValues = [0, 0];
+var tempValues = [0, 0];
 
 async function fetchDataJSON() {
   const daysAgo = document.getElementById("days-select").value;
   const deviceName = document.getElementById("device-select").value;
   console.log("Fetching: daysAgo=" + daysAgo + ", device=" + deviceName);
-  const responseBody = await fetch("https://foj972s0id.execute-api.eu-central-1.amazonaws.com/prod/stats4?daysago=" + daysAgo + "&device=" + deviceName, { 
+  const responseBody = await fetch("https://foj972s0id.execute-api.eu-central-1.amazonaws.com/prod/stats4?daysago=" + daysAgo + "&device=" + deviceName, {
       method: "GET",
-      // mode: "cors", // cors, no-cors, same-origin
-      origin: "*", // * or specific origin
+      origin: "*",
   })
     .then(response => {
       if (!response.ok) {
@@ -29,14 +29,13 @@ function updateChart(responseData) {
   timeLabels = dataObj.data.battery.map((item) => timestampMillisToCurrentTime(item.time));
 
   batteryValues = dataObj.data.battery.map((item) => mapNumRange(item.value, 3300, 4200, 0, 100));
-  moisture1Values = dataObj.data.moisture_1.map((item) => item.value);
-  moisture2Values = dataObj.data.moisture_2.map((item) => item.value);
+  moistureValues = dataObj.data.moisture.map((item) => item.value);
+  waterAvailableValues = dataObj.data.water_available.map((item) => item.value ? 100 : 0);
   wateredValues = dataObj.data.watered.map((item) => item.value ? 100 : 0);
   tempValues = dataObj.data.temp.map((item) => item.value);
   tempValues = tempValues.map((item) => item == -127 ? 0 : item);
 
-  // if all arrays are empty, return
-  if (batteryValues.length === 0 && moisture1Values.length === 0 && moisture2Values.length === 0 && wateredValues.length === 0 && tempValues.length === 0) {
+  if (batteryValues.length === 0 && moistureValues.length === 0 && wateredValues.length === 0 && tempValues.length === 0) {
     document.getElementById("chart").innerHTML = "No data";
     return;
   }
@@ -46,7 +45,6 @@ function updateChart(responseData) {
   getLastBatteryData();
   getLastTemperature();
 
-  // frappe line chart
   const graphData = {
     labels: timeLabels,
     datasets: [
@@ -56,14 +54,9 @@ function updateChart(responseData) {
         values: batteryValues
       },
       {
-        name: "moisture_1",
+        name: "moisture",
         chartType: "line",
-        values: moisture1Values
-      },
-      {
-        name: "moisture_2",
-        chartType: "line",
-        values: moisture2Values
+        values: moistureValues
       },
       {
         name: "watered",
@@ -74,23 +67,23 @@ function updateChart(responseData) {
         name: "temp",
         chartType: "line",
         values: tempValues
+      },
+      {
+        name: "water available",
+        chartType: "bar",
+        values: waterAvailableValues
       }
     ],
     yMarkers: [{ label: "30°C", value: 30, options: { labelPos: "left" } }],
-    // yRegions: [
-    //   { label: "Region", start: -10, end: 50, options: { labelPos: "right" } }
-    // ]
   }
 
-  const chart = new frappe.Chart("#chart", {  // or a DOM element,
-    // new Chart() in case of ES6 module with above usage
+  const chart = new frappe.Chart("#chart", {
     title: "Humidity and Battery Level",
     data: graphData,
-    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+    type: 'axis-mixed',
     height: 600,
-    // valuesOverPoints: 1,
     truncateLegends: true,
-    colors: ["#30b455", "#97E7E1", "#6AD4DD", "#0000FF", "#FF7700"],
+    colors: ["#30b455", "#97E7E1", "#0000FF", "#FF7700", "#6AD4DD"],
     axisOptions: {
       xAxisMode: "tick",
       xIsSeries: true,
@@ -102,12 +95,10 @@ function updateChart(responseData) {
     },
     lineOptions: {
       hideDots: 0,
-      dotSize: 2, // default: 4
-      spline: 1, // default: 0
-      // heatline: 1
+      dotSize: 2,
+      spline: 1,
     },
     barOptions: {
-      // stacked: true,
       spaceRatio: 0.95
     },
     tooltipOptions: {
@@ -117,18 +108,12 @@ function updateChart(responseData) {
   })
 
   function getLastMoistureData() {
-    var lastMoisture1 = moisture1Values[moisture1Values.length - 1];
-    var lastMoisture2 = moisture2Values[moisture2Values.length - 1];
-
-    document.getElementById("moisture_1").innerHTML = lastMoisture1 + "%";
-    document.getElementById("moisture_2").innerHTML = lastMoisture2 + "%";
-
-    document.getElementById("moisture_1_emoji").innerHTML = getMoistureEmoji(lastMoisture1);
-    document.getElementById("moisture_2_emoji").innerHTML = getMoistureEmoji(lastMoisture2);
+    var lastMoisture = moistureValues[moistureValues.length - 1];
+    document.getElementById("moisture").innerHTML = lastMoisture + "%";
+    document.getElementById("moisture_emoji").innerHTML = getMoistureEmoji(lastMoisture);
   }
 
   function renderLastUpdatesTime() {
-    // set last updated time
     if (dataObj.data.battery.length === 0) {
       document.getElementById("last-updated-time").innerHTML = "no data";
       return;
@@ -136,7 +121,6 @@ function updateChart(responseData) {
     var lastBatteryTime = dataObj.data.battery[dataObj.data.battery.length - 1].time;
     document.getElementById("last-updated-time").innerHTML = timestampMillisToCurrentTime(lastBatteryTime);
 
-    // set last watered
     const lastWateredIndex = wateredValues.lastIndexOf(100);
     if (lastWateredIndex !== -1) {
       const lastWateredTime = timeLabels[lastWateredIndex];
@@ -144,8 +128,7 @@ function updateChart(responseData) {
     } else {
       document.getElementById("last-watered-time").innerHTML = "no data";
     }
-  
-    // Set the max temperature value
+
     const maxTemperature = Math.max(...tempValues);
     document.getElementById("max-temperature").innerHTML = `${maxTemperature}°C`;
   }
@@ -169,8 +152,7 @@ function fetchAndUpdateChart() {
   });
 }
 
-fetchAndUpdateChart(); // do the first fetch
-// setup fetch on change
+fetchAndUpdateChart();
 document.getElementById("days-select").addEventListener("change", fetchAndUpdateChart);
 document.getElementById("device-select").addEventListener("change", fetchAndUpdateChart);
 
@@ -189,24 +171,17 @@ function mapNumRange(num, inMin, inMax, outMin, outMax) {
 }
 
 function timestampMillisToCurrentTime(dateTime) {
-  //conver date time like '1717326306266' to ISO8601 format
   var localeDateTime = new Date(dateTime);
-
-  // Extract hours, minutes, day, and month
-  var hours = localeDateTime.getHours().toString().padStart(2, '0'); // Ensure 2 digits
-  var minutes = localeDateTime.getMinutes().toString().padStart(2, '0'); // Ensure 2 digits
-  var day = localeDateTime.getDate().toString().padStart(2, '0'); // Ensure 2 digits
-  var month = (localeDateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-based
-
-  // Format as HH:MM DD.MM
+  var hours = localeDateTime.getHours().toString().padStart(2, '0');
+  var minutes = localeDateTime.getMinutes().toString().padStart(2, '0');
+  var day = localeDateTime.getDate().toString().padStart(2, '0');
+  var month = (localeDateTime.getMonth() + 1).toString().padStart(2, '0');
   return `${hours}:${minutes} ${day}.${month}`;
-
-  // return new Date(dateTime).toTimeString().substring(0, 5); // get only first 5 characters
 }
 
 function getMoistureEmoji(value) {
-  if (value < 25) return "🏜️"; // Very dry
-  if (value < 70) return "💧"; // Moderately dry
-  if (value < 90) return "💦"; // Good moisture
-  return "🌊"; // High moisture
+  if (value < 25) return "🏜️";
+  if (value < 70) return "💧";
+  if (value < 90) return "💦";
+  return "🌊";
 }
--- garden-bot/lambda-timestream-query.py
+++ garden-bot/lambda-timestream-query.py
@@ -10,50 +10,44 @@ table = dynamodb.Table("garden-bot-data")
 print('Loading function')
 
 def lambda_handler(event, context):
-    
+
     query_params = event.get('queryStringParameters') or {}
     daysAgo = query_params.get('daysago')
-    # print("event: " + str(event))
-    # print("queryParams: " + str(query_params))
     if (not daysAgo) or (not daysAgo.isdigit()) or int(daysAgo) > 30 or int(daysAgo) < 1:
         daysAgo = 1
 
     daysAgo = int(daysAgo)
-    
-    # Calculate the timestamp to look up from till now
+
     now = datetime.utcnow()
     start_timestamp = now - timedelta(days=daysAgo)
     start_timestamp_precise = int(start_timestamp.timestamp()) * 1000
 
     device_name = query_params.get('device')
     if not device_name:
-        device_name = 'veso-cherry-watcher'
+        device_name = 'cherry-3-pot'
 
-    # print("will query db for: " + device_name + " from " + str(daysAgo) + " days ago")
-    
     response = table.query(
         KeyConditionExpression=Key('device').eq(device_name) & Key('timestamp').gt(start_timestamp_precise)
     )
 
     rows = response['Items']
-    # print("rows: " + str(len(rows)))
 
     battery_points = []
-    moisture_1_points = []
-    moisture_2_points = []
+    moisture_points = []
+    water_available_points = []
     watered_points = []
     temp_points = []
     for row in rows:
         battery_points.append(rowToObj(row, 'battery'))
-        moisture_1_points.append(rowToObj(row, 'moisture_1'))
-        moisture_2_points.append(rowToObj(row, 'moisture_2'))
+        moisture_points.append(rowToObj(row, 'moisture'))
+        water_available_points.append(rowToObj(row, 'water_available'))
         watered_points.append(rowToObj(row, 'watered'))
         temp_points.append(rowToObj(row, 'temp'))
 
     pivoted_rows = {
        "battery": battery_points,
-       "moisture_1": moisture_1_points,
-       "moisture_2": moisture_2_points,
+       "moisture": moisture_points,
+       "water_available": water_available_points,
        "watered": watered_points,
        "temp": temp_points
     }
@@ -72,7 +66,7 @@ def lambda_handler(event, context):
 
 
 def rowToObj(row, measurementName):
-    measurement_value = row.get('measurements').get(measurementName, 0)  # Default to 0 if key is missing
+    measurement_value = row.get('measurements').get(measurementName, 0)
     timestamp_value = row.get('timestamp')
     device_value = row.get('device')
 
